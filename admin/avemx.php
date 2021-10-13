77923b22ad0e98f6efb1e32a06ef9676d6e07958
LogonSafe <76534153+LogonSafe@users.noreply.github.com>
2021-10-13 18:21:24 -0400

    clear again

--- admin/avemx.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$this_fund_symbol = 'avemx';
-$this_fund_name = 'Ave Maria Value Fund';
-$this_page = $_SERVER['PHP_SELF'];
-
-include("fund.inc.php");
-
-?>
--- admin/avewx.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$this_fund_symbol = 'avewx';
-$this_fund_name = 'Ave Maria World Equity Fund';
-$this_page = $_SERVER['PHP_SELF'];
-
-include("fund.inc.php");
-
-?>
