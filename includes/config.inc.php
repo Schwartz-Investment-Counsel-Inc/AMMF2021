ee3b616529e3db306e3ff038b2780482f4326131
g.malcolm <g.malcolm@studio228.com>
2023-05-23 11:11:02 -0400

    Update config.inc.php

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -8,7 +8,7 @@
   define('MYSQL_PASSWORD', 'Rw!s3kjh#5ET81Dy');
   define('ALPHA_VANTAGE_KEY', 'A8FUJQ3P047IL8IN'); // +amprod
   define('FINANCIAL_ADVISOR_EMAIL_ADDRESSES', FALSE);
-  define('CONTACT_SUBMITTED_EMAIL_ADDRESSES', 'g.malcolm@studio228.com, mjr@schwartzinvest.com, keh@schwartzinvest.com, corourke@schwartzinvest.com, kmf@schwartzinvest.com');
+  define('CONTACT_SUBMITTED_EMAIL_ADDRESSES', 'g.malcolm@studio228.com, mjr@schwartzinvest.com, keh@schwartzinvest.com, corourke@schwartzinvest.com, jig@schwartzinvest.com');
   define('TECH_SUPPORT_EMAIL_ADDRESSES', 'support@theholdeng.com, rcs@schwartzinvest.com, mjr@schwartzinvest.com, g.malcolm@studio228.com');
   define('EMAIL_SUBJECT_PREFIX', FALSE);
 ?>
